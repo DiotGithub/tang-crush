42424af3322de37bff36c25441b291624d04f9ac
Việt Anh <95431417+DiotGithub@users.noreply.github.com>
2021-12-02 23:48:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chin Chào Bé :>',
+    introDesc: `Anh có điều này muốn hỏi bé, nhưng phải trả lời thật lòng nhó !`,
+    btnIntro: 'Oke nà !',
+    title: 'Này bé gì owi, bé có yêu anh hông 💕',
+    desc: 'Bé không trả lời hoặc thoát ra, là bé là người eo của anh đó nha :>',
+    btnYes: 'Có 💜',
+    btnNo: 'ĐÉO :)',
+    question:'Nói cho anh biết lý do vì sao bé yêu anh điii ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Tại vì cậu đẹp trai hihi <333333',
+    mess: 'Anh biết mà :>, Yew bé x4.19 lần 💕',
+    messDesc: 'Còn chờ gì nữa mà không inbox cho anh đy nhò =))',
+    btnAccept: 'Tới luôn nà ❤',
+    messLink: 'https://www.facebook.com/vietanh.com.vns'
+}
