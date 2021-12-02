a5d4caf652591b19341fdc6b1b6e43557fe8e429
DiotGithub <VietAnh.Com.Vns@gmail.com>
2021-12-02 23:25:44 +0700

    Tang-Crush

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Chin Chào Bé :>',
+    introDesc: `Anh có điều này muốn hỏi bé, nhưng phải trả lời thật lòng nhó !`,
+    btnIntro: 'Oke nà !',
+    title: 'Này bé gì owi, bé có yêu anh hông 💕',
+    desc: 'Bé không trả lời hoặc thoát ra, là bé là người eo của anh đó nha :>',
+    btnYes: 'Có 💜',
+    btnNo: 'ĐÉO :)',
+    question:'Nói cho anh biết lý do vì sao bé yêu anh điii ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Tại vì cậu đẹp trai hihi <333333',
+    mess: 'Anh biết mà :>, Yew bé x4.19 lần 💕',
+    messDesc: 'Còn chờ gì nữa mà không inbox cho anh đy nhò =))',
+    btnAccept: 'Tới luôn nà ❤',
+    messLink: 'https://www.facebook.com/vietanh.com.vns', //link fb
+    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
